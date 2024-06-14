fe47f80c6290beecc0e81ffaa96c93cb0bfa816d
kartik kohapare <kartik@kartiks-MacBook-Air.local>
2024-06-14 18:36:52 +0530

    buil first module

---
+++ projectmanagementsystem/src/main/java/com/kartik/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.kartik.config;
+
+public class JwtConstant {
+    public static final String SECRETE_KEY = "KAJTKEJWIOEJKJWJKAJK AKJKDJJASJKJKA KJAKJDJKJAKFJDKJAKJDJAJJKDJJAA AKAD";
+    
+    public static final String JWT_HEADER = "Authorization";
+}
---
+++ projectmanagementsystem/src/main/java/com/kartik/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.kartik.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kartik.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+  
+	
+	User findByEmail(String email);
+	
+}
